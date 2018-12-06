d45a07a2f1c8b2ed84a1aa388c3d63397034b6f0
Saikrishna41 <saai.krrishna@gmail.com>
2018-12-06 09:24:05 -0600

    Date to string extension at line 293

--- FreshList/AppDelegate.swift
+++ FreshList/AppDelegate.swift
@@ -290,4 +290,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDe
     }
 
 }
+extension Date
+{
+    func toString( dateFormat format  : String ) -> String
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: self)
+    }
+    
+}
 
